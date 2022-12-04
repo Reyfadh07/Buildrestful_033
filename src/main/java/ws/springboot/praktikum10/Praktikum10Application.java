6c21f2ac5f99162109532e8ae56762a32bb5e5f6
Reyfadh07 <raihan.farmasi@gmail.com>
2022-12-04 18:30:02 +0700

    first commit

---
+++ src/main/java/ws/springboot/praktikum10/Praktikum10Application.java
@@ -0,0 +1,13 @@
+package ws.springboot.praktikum10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktikum10Application.class, args);
+	}
+
+}
---
+++ src/test/java/ws/springboot/praktikum10/Praktikum10ApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.springboot.praktikum10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
